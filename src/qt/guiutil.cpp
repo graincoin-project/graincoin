624bb235b96191d077d45c2d68400e24cac0feef
bee7 <bee7@users.noreply.github.com>
2017-04-26 08:44:18 +0300

    Rebranding: sample address
    The sample address replaced with artificial 33-byte all-ones pubkey

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -115,7 +115,7 @@ void setupAddressWidget(QValidatedLineEdit *widget, QWidget *parent)
 #if QT_VERSION >= 0x040700
     // We don't want translators to use own addresses in translations
     // and this is the only place, where this address is supplied.
-    widget->setPlaceholderText(QObject::tr("Enter a Graincoin address (e.g. %1)").arg("1NS17iag9jJgTHD1VXjvLCEnZuQ3rJDE9L"));
+    widget->setPlaceholderText(QObject::tr("Enter a Graincoin address (e.g. %1)").arg("97WH4vr3mdwkrdVcvRaCcfLZ9VRZcdoCRw"));
 #endif
     widget->setValidator(new BitcoinAddressEntryValidator(parent));
     widget->setCheckValidator(new BitcoinAddressCheckValidator(parent));
